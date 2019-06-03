d1ce5f49c9124ae8a7631b600b7dabedfa072e48
Christoph Goller <st149324@stud.uni-stuttgart.de>
2019-06-03 23:09:27 +0200

    Fixed complaint query

--- frontend/src/components/Issues/Issues.js
+++ frontend/src/components/Issues/Issues.js
@@ -45,7 +45,7 @@ class Issues extends Component {
       this.setState({ active: this.state.issues.filter((a) => a.id === issue.id)[0] });
     }
     componentDidMount = () => {
-      this.fetchData({ limit: 20, offset: 0 });
+      this.fetchData({ count: 20 });
     }
     render () {
       return (
@@ -93,7 +93,7 @@ class Issues extends Component {
                     ? (
                       <i className='fa fa-spinner' />
                     ) : (
-                      <Table data={this.state.issues} onClick={this.activate} tags={['id', 'preview', 'receiveDate', 'sentiment', 'subject']} sticky/>
+                      <Table data={this.state.issues} onClick={this.activate} tags={['id', 'preview', 'receiveDate', 'sentiment', 'subject']} sticky />
                     )}
                 </div>
               </Body>
