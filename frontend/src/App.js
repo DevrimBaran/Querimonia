bf1b0df3d43618104db619ee91a389b9fed88a7a
Koß <jonas.koss@iao.fraunhofer.de>
2019-06-05 15:29:06 +0200

    failed to push...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import Menu from "components/Menu/Menu";
+import Menu from 'components/Menu/Menu';
 
-import Home from "views/Home";
-import Complaints from "views/Complaints";
-import Statistics from "views/Statistics";
-import WordVectors from "views/WordVectors";
+import Home from 'views/Home';
+import Complaints from 'views/Complaints';
+import Statistics from 'views/Statistics';
+import WordVectors from 'views/WordVectors';
 
-function App() {
+function App () {
   return (
     <Router>
       <Menu />
 
-      <Route exact path="/" component={Home} />
-      <Route path="/complaints" component={Complaints} />
-      <Route path="/statistics" component={Statistics} />
-      <Route path="/wordvectors" component={WordVectors} />
+      <Route exact path='/' component={Home} />
+      <Route path='/complaints' component={Complaints} />
+      <Route path='/statistics' component={Statistics} />
+      <Route path='/wordvectors' component={WordVectors} />
     </Router>
   );
 }
