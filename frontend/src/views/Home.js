2838a94a22f90ad8721278818481bfcf1ff14e87
Josef Tunc <josef.arsan@gmail.com>
2019-06-06 17:09:53 +0200

    Startseite bearbeitet

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,12 +1,44 @@
 import React from 'react';
 
 import Block from 'components/Block/Block';
+import logo from './../assets/img/StuproLogo2.svg';
+import team from './../assets/img/Gruppenfoto.jpg';
+import iaoPartner from './../assets/img/iao.svg';
+import iatPartner from './../assets/img/iat.svg';
+
+import './Home.scss';
 
 function Home () {
   return (
     <React.Fragment>
       <Block>
-        <h6 className='center'>Hallo :)</h6>
+        <div className='header'>
+          <img src={logo} className='Topbar-logo' alt='logo' width='60%' />
+        </div>
+        <div className='body'>
+          <br></br>
+          <p>
+          Im Rahmen des Studienprojektes der Universität Stuttgart im Sommersemester 2019
+          wurde am IAT der Universität Stuttgart und dem Fraunhofer IAO die webbasierte
+          Plattform Querimonia für die Erprobung und Anwendung von Verfahren zur Analyse
+          und Aufbereitung von Texten am Beispiel des Beschwerdemanagements entwickelt.
+          </p>
+          <br></br>
+          <p>
+          Ein typisches Anwendungsbeispiel ist die automatische Generierung einer Antwort
+          auf eine Beschwerdenachricht mit Hilfe von Mensch-Maschine-Kollaboration mit
+          selbstlernenden Textminingverfahren.
+          </p>
+          <br></br>
+          <h1>Unser Team</h1>
+          <img src={team} className='Topbar-logo' alt='logo' width='50%' />
+          <p>&copy; 2019 Fraunhofer IAO, IAT Universität Stuttgart</p>        
+        </div>
+        <div className='footer'>
+          <h1>Unsere Partner</h1>
+          <img src={iaoPartner} className='iaoPartner' alt='logo' width='40%' />
+          <img src={iatPartner} className='iatPartner' alt='logo' width='40%' />
+        </div>
       </Block>
     </React.Fragment>
   );
