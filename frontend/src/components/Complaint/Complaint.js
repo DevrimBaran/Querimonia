0ad40518152a9f1f8f861e34bdbd0fd6f75bd26d
koss <inf90370@stud.uni-stuttgart.de>
2019-06-06 03:52:41 +0200

    ...

--- frontend/src/components/Complaint/Complaint.js
+++ frontend/src/components/Complaint/Complaint.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+function foo() {
+    //wird für Link benötigt, für husky allerdings unused...
+    Router();
+    Route();
+}
+
 function getMaxKey (obj) {
   let max = -1;
   let value = '';
