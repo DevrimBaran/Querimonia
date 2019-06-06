cbd80bd40ee0385deb65f3c125ad559e62ddc8f3
PaulBredl <st156256@stud.uni-stuttgart.de>
2019-06-06 10:32:53 +0200

    Fixed bug with money amount, changed entitiy labels and formatted code

--- backend/src/main/java/de/fraunhofer/iao/querimonia/nlp/extractor/KikukoExtractor.java
+++ backend/src/main/java/de/fraunhofer/iao/querimonia/nlp/extractor/KikukoExtractor.java
@@ -1,13 +1,9 @@
 package de.fraunhofer.iao.querimonia.nlp.extractor;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import de.fraunhofer.iao.querimonia.nlp.KikukoConatct;
 import de.fraunhofer.iao.querimonia.nlp.NamedEntity;
 import de.fraunhofer.iao.querimonia.rest.restobjects.kikuko.ExtractorPipelines;
 import de.fraunhofer.iao.querimonia.rest.restobjects.kikuko.ExtractorResponse;
-import de.fraunhofer.iao.querimonia.rest.restobjects.kikuko.KikukoResponse;
-import de.fraunhofer.iao.querimonia.rest.restobjects.kikuko.Pipelines;
-import jdk.nashorn.internal.ir.debug.JSONWriter;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -15,25 +11,33 @@ import java.util.List;
 public class KikukoExtractor extends KikukoConatct<ExtractorResponse> implements EntityExtractor {
 
   public KikukoExtractor() {
-      super("domain", "QuerimoniaExtract");
+    super("domain", "QuerimoniaExtract");
   }
 
   @Override
   public List<NamedEntity> extractEntities(String text) {
-      ExtractorResponse response = executeKikukoRequest(text, ExtractorResponse[].class);
-      ExtractorPipelines allPipes = response.getPipelines();
+    ExtractorResponse response = executeKikukoRequest(text, ExtractorResponse[].class);
+    ExtractorPipelines allPipes = response.getPipelines();
 
-      List<NamedEntity> entities = new LinkedList<>();
+    List<NamedEntity> entities = new LinkedList<>();
 
-      allPipes.getBusInformationen().forEach(e -> entities.add(new NamedEntity("Bushaltestelle", e.getStartposition(),e.getEndposition())));
-      allPipes.getDatumExtraktor().forEach(e -> entities.add(new NamedEntity("Datum", e.getStartposition(),e.getEndposition())));
-      allPipes.getExtdatumExtraktor().forEach(e -> entities.add(new NamedEntity("Datum", e.getStartposition(),e.getEndposition())));
-      allPipes.getMoneyAmount().forEach(e -> entities.add(new NamedEntity("Money Amount", e.getStartposition(),e.getEndposition())));
-      allPipes.getExtpersonExtraktor().forEach(e -> entities.add(new NamedEntity("Person", e.getStartposition(),e.getEndposition())));
-      allPipes.getPersonExtraktor().forEach(e -> entities.add(new NamedEntity("Person", e.getStartposition(),e.getEndposition())));
-      allPipes.getPersonNeu().forEach(e -> entities.add(new NamedEntity("Person", e.getStartposition(),e.getEndposition())));
+    allPipes.getBusInformationen().forEach(e -> entities.add(new NamedEntity("Bushaltestelle",
+        e.getStartposition(), e.getEndposition())));
+    allPipes.getDatumExtraktor().forEach(e -> entities.add(new NamedEntity("Datum",
+        e.getStartposition(), e.getEndposition())));
+    allPipes.getExtdatumExtraktor().forEach(e -> entities.add(new NamedEntity("Datum",
+        e.getStartposition(), e.getEndposition())));
+    // for some reason we have to subtract 1 from the end position here
+    allPipes.getMoneyAmount().forEach(e -> entities.add(new NamedEntity("Geldbetrag",
+        e.getStartposition(), e.getEndposition() - 1)));
+    allPipes.getExtpersonExtraktor().forEach(e -> entities.add(new NamedEntity("Name",
+        e.getStartposition(), e.getEndposition())));/*
+    allPipes.getPersonExtraktor().forEach(e -> entities.add(new NamedEntity("Name",
+        e.getStartposition(), e.getEndposition() - 1)));*//*
+    allPipes.getPersonNeu().forEach(e -> entities.add(new NamedEntity("Name",
+        e.getStartposition(), e.getEndposition() - 1)));*/
 
-      return entities;
+    return entities;
   }
 
 }
