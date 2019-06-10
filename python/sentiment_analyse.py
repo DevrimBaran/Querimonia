d9dffef6173694998c4e796f114ca50e028ff952
SonBoolean <45284935+SonBoolean@users.noreply.github.com>
2019-06-10 13:18:43 +0200

    added new senti skript and senti txt data

---
+++ python/sentiment_analyse.py
@@ -0,0 +1,111 @@
+import string
+import sys
+import nltk
+import argparse
+
+
+# Sarcasm, Irony not detected. For example: 'Ich hasse dich und liebe dich nicht' has value > 0
+
+
+def negative_sentiment(query):
+    tokens = filter_query(query)
+    sentiment_value_negative = 0
+
+    for j in tokens:
+        with open('SentiWS_v2.0_Negative.txt', 'r' , encoding='utf-8') as sentis:
+            for s in sentis:
+                cells = s.split('\t')
+                lemma = cells[0].split('|')[0]
+
+                value = float(cells[1].strip())
+
+                infl = cells[2].split(',')
+                # delete the \n in the last word
+                infl[len(infl) - 1] = infl[len(infl) - 1].strip()
+                # loop over inflections
+                for i in infl:
+                    # if token matches with one of infl.
+                    if j == i:
+                        #print(j, '&', value)
+                        sentiment_value_negative += value
+                # if token matches with lemma
+                if j == lemma:
+                    #print(j, '&', value)
+                    sentiment_value_negative += value
+
+    return sentiment_value_negative
+
+
+def positive_sentiment(query):
+    tokens = filter_query(query)
+    sentiment_value_positive = 0
+
+    for j in tokens:
+        with open('SentiWS_v2.0_Positive.txt', 'r' , encoding='utf-8') as sentis:
+            for s in sentis:
+                cells = s.split('\t')
+                lemma = cells[0].split('|')[0]
+
+                value = float(cells[1].strip())
+
+                infl = cells[2].split(',')
+                # delete the \n in the last word
+                infl[len(infl) - 1] = infl[len(infl) - 1].strip()
+                # loop over inflections
+                for i in infl:
+                    # if token matches with one of infl.
+                    if j == i:
+                        #print(j, '&', value)
+                        sentiment_value_positive += value
+                # if token matches with lemma
+                if j == lemma:
+                    #print(j, '&', value)
+                    sentiment_value_positive += value
+
+    return sentiment_value_positive
+
+
+def filter_query(query):
+    # tokenize, erase punctuation(#, $, %, ., ~, ...), erase whitespaces
+    tokens = nltk.tokenize.word_tokenize(query, 'german')
+    tokens = [''.join(i for i in s if i not in string.punctuation)
+              for s in tokens]
+    tokens = list(filter(None, tokens))
+    return tokens
+
+
+# parsing params von command line
+def parse_arg():
+    sentiment_value = 0
+    for arg in sys.argv[1:]:
+        sentiment_value += positive_sentiment(arg) + negative_sentiment(arg)
+    return sentiment_value
+
+
+# for flask
+def main(query):
+    return positive_sentiment(query) + negative_sentiment(query)
+
+
+
+
+if __name__ == '__main__':
+
+    test_pos = "Ich mag es mit der Bahn zu fahren und liebe Zugausfälle "
+    test_neg = "Ich hasse Zerstörung und Gewalt"
+
+    #sentiment_value = positive_sentiment(test_neg) + negative_sentiment(test_neg)
+    #print(sentiment_value)
+
+    #command line print
+    #print("Sentiment Value is: " + (str)(parse_arg()))
+
+
+
+
+
+
+
+
+
+
