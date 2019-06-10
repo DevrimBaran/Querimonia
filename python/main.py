6cd088103d82223011fb4d9827ef467d5ee8115a
SonBoolean <45284935+SonBoolean@users.noreply.github.com>
2019-06-10 13:43:31 +0200

    small fix

--- python/main.py
+++ python/main.py
@@ -5,7 +5,7 @@ import matplotlib_generator
 import sentiment
 
 app = Flask(__name__)
-@app.route('/sentiment', methods=['POST'])
+@app.route('/sentiment_analyse', methods=['POST'])
 # TODO correct Rest API with post, get
 def return_sentiment():
     # Example: python -c "import requests; res = requests.post('http://localhost:5000/sentiment', json={'text':'Ich hasse Hawaiipizza'}); print(res.json())"
@@ -13,8 +13,8 @@ def return_sentiment():
     content = request.get_json()
     query = content['text']
     # TODO add SentiWS_v2.0_Negative.txt then uncomment this
-    #sentiment_value = sentiment.positive_sentiment(query)+sentiment.negative_sentiment(query)
-    sentiment_value = 0.5
+    sentiment_value = main(query)
+
     return jsonify({"sentiment": sentiment_value})
 
 if __name__ == '__main__':
