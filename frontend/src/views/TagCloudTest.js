71235e1c7b6b743d4588ed3f7148922193a87be3
Schweinebaermann <34004884+Schweinebaermann@users.noreply.github.com>
2019-06-11 18:02:55 +0200

    More filters and relative size of the tags

--- frontend/src/views/TagCloudTest.js
+++ frontend/src/views/TagCloudTest.js
@@ -7,29 +7,38 @@ import Api from 'utility/Api';
 
 import './style.css';
 
-
 class TagCloudTest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      words: {}
+      words: {},
+      max:0
     };
   }
   fetchData = () => {
     let query = {};
     this.refs.minDate.value && (query.date_min = this.refs.minDate.value);
     this.refs.maxDate.value && (query.date_max = this.refs.maxDate.value);
+    this.refs.onlyWords.checked && (query.words_only = this.refs.onlyWords.checked);
+    this.refs.count.value && (query.count = this.refs.count.value);
     Api.get('/api/stats/tagcloud', query)
       .then(this.setData);
   }
   setData = (data) => {
     this.setState({words: data});
+    var maxVal=0;
+    for(var i=0;i<Object.keys(Object.keys(data)).length;i++){
+      if(maxVal<this.state.words[Object.keys(data)[i]])
+        maxVal=this.state.words[Object.keys(data)[i]];
+    }
+    this.setState({max: maxVal});
   }
   componentDidMount () {
     this.fetchData();
   }
   renderWord = (word, index) => {
-    return (<div key={index} style={{ fontSize: this.state.words[word] * 10 }}>{word}</div>);
+    //declaration of the tags with relative size
+    return (<div key={index} style={{ fontSize: (this.state.words[word]/this.state.max) * 150 }}>{word}</div>);
   }
   render () {
     return (
@@ -40,6 +49,10 @@ class TagCloudTest extends Component {
           <input type="date" id="minDate" ref="minDate" />
           <label htmlFor="maxDate">Bis:</label>
           <input type="date" id="maxDate" ref="maxDate" />
+          <label htmlFor="onlyWords">Nur Wörter anzeigen:</label>
+          <input type="checkbox" id="onlyWords" ref="onlyWords" />
+          <label htmlFor="count">Anzahl der Tags:</label>
+          <input type="number" id="count" ref="count" />
           <input type="button" onClick={this.fetchData} value="aktualisieren" />
           <TagCloud
             className='tag-cloud'
