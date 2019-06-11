e51cde830d5da106203a438b84fd6e9690c0d947
koss <inf90370@stud.uni-stuttgart.de>
2019-06-11 21:29:19 +0200

    TagCloud enhancements

--- frontend/src/views/TagCloudTest.js
+++ frontend/src/views/TagCloudTest.js
@@ -1,61 +1,112 @@
 import React, { Component } from 'react';
 import randomColor from 'randomcolor';
 import TagCloud from 'react-tag-cloud';
-// import CloudItem from './CloudItem';
+import Block from 'components/Block/Block';
+import Row from 'components/Row/Row';
+import Content from 'components/Content/Content';
 
 import Api from 'utility/Api';
 
 import './style.css';
 
-
 class TagCloudTest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      words: {}
+      words: {},
+      color: '#179c7d',
+      factor: 10,
+      size: 5,
     };
   }
   fetchData = () => {
     let query = {};
     this.refs.minDate.value && (query.date_min = this.refs.minDate.value);
     this.refs.maxDate.value && (query.date_max = this.refs.maxDate.value);
+    this.refs.onlyWords.checked && (query.words_only = this.refs.onlyWords.checked);
+    this.refs.count.value && this.refs.count.value > 0 && (query.count = this.refs.count.value);
     Api.get('/api/stats/tagcloud', query)
+      .then(a => { console.log(a); return a; })
       .then(this.setData);
   }
   setData = (data) => {
-    this.setState({words: data});
+    this.setState({ words: data });
   }
-  componentDidMount () {
+  onChange = (e) => {
+    let s = {};
+    s[e.target.id] = e.target.value;
+    this.setState(s);
+  }
+  componentDidMount() {
     this.fetchData();
   }
   renderWord = (word, index) => {
-    return (<div key={index} style={{ fontSize: this.state.words[word] * 10 }}>{word}</div>);
+    return (<abbr key={index} title={this.state.words[word]} style={{ fontSize: this.state.size - 2 + this.state.words[word] * this.state.factor }}>{word}</abbr>);
   }
-  render () {
+  render() {
     return (
-      <div className='app-outer'>
-        <div className='app-inner'>
-          <h1>Wortliste</h1>
-          <label htmlFor="minDate">Von:</label>
-          <input type="date" id="minDate" ref="minDate" />
-          <label htmlFor="maxDate">Bis:</label>
-          <input type="date" id="maxDate" ref="maxDate" />
-          <input type="button" onClick={this.fetchData} value="aktualisieren" />
-          <TagCloud
-            className='tag-cloud'
-            style={{
-              fontFamily: 'sans-serif',
-              // fontSize: () => Math.round(Math.random() * 50) + 16,
-              fontSize: 30,
-              color: () => randomColor({
-                hue: 'blue'
-              }),
-              padding: 5
-            }}>
-            {Object.keys(this.state.words).map(this.renderWord)}
-          </TagCloud>
-        </div>
-      </div>
+      <React.Fragment>
+        <Block>
+          <Row vertical={true}>
+            <h1>Wortliste</h1>
+            <br />
+            <div>
+              <Row vertical={false} style={{ justifyContent: 'space-around' }}>
+                <div>
+                  <label htmlFor='minDate'>Von:</label><br />
+                  <input type='date' id='minDate' ref='minDate' />
+                </div>
+                <div>
+                  <label htmlFor='maxDate'>Bis:</label><br />
+                  <input type='date' id='maxDate' ref='maxDate' />
+                </div>
+                <div>
+                  <label htmlFor="onlyWords">Nur Wörter anzeigen:</label><br />
+                  <input type="checkbox" id="onlyWords" ref="onlyWords" />
+                </div>
+                <div>
+                  <label htmlFor="count">Wortanzahl:</label><br />
+                  <input type="number" id="count" ref="count" defaultValue="0" min="0" />
+                </div>
+              </Row>
+            </div>
+            <div className="center">
+              <input type='button' onClick={this.fetchData} value='aktualisieren' />
+            </div>
+            <Content>
+              <TagCloud
+                className='tag-cloud'
+                style={{
+                  fontFamily: 'sans-serif',
+                  color: () => randomColor({
+                    hue: this.state.color
+                  })
+                }}>
+                {Object.keys(this.state.words).map(this.renderWord, this)}
+              </TagCloud>
+            </Content>
+            <div>
+              <Row vertical={false} style={{ justifyContent: 'center' }}>
+                <div>
+                  <label htmlFor='color'>Farbe:</label><br />
+                  <input type='color' id="color" onChange={this.onChange} value={this.state.color} />
+                </div>
+                <div style={{ width: '2em' }}></div>
+                <div>
+                  <label htmlFor='factor'>Schriftgröße:</label><br />
+                  <input type='number' id="size" onChange={this.onChange} value={this.state.size} />
+                </div>
+                <div style={{ width: '2em' }}></div>
+                <div>
+                  <label htmlFor='factor'>Faktor:</label><br />
+                  <input type='number' id="factor" onChange={this.onChange} value={this.state.factor} />
+                </div>
+              </Row>
+            </div>
+            <br />
+          </Row>
+        </Block>
+      </React.Fragment>
     );
   }
 }
