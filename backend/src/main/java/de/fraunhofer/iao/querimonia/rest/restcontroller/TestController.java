f6c91c87158495ce032f27c322776f8eba804eb4
PaulBredl <st156256@stud.uni-stuttgart.de>
2019-07-04 19:49:38 +0200

    Test for file path

--- backend/src/main/java/de/fraunhofer/iao/querimonia/rest/restcontroller/TestController.java
+++ backend/src/main/java/de/fraunhofer/iao/querimonia/rest/restcontroller/TestController.java
@@ -14,7 +14,7 @@ import java.io.File;
 public class TestController {
 
   @GetMapping("/api/test")
-  public File[] test() {
-    return new File("").listFiles();
+  public String[] test() {
+    return new File("").getAbsoluteFile().list();
   }
 }
