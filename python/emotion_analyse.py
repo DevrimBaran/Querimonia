85163d433370af67bd5c372fd059e315071d5c35
SonBoolean <45284935+SonBoolean@users.noreply.github.com>
2019-07-09 16:49:51 +0200

    works but stil in progress

--- python/emotion_analyse.py
+++ python/emotion_analyse.py
@@ -1,10 +1,16 @@
 import string
 import nltk
 from enum import Enum
-
+from pathlib import Path
+from collections import defaultdict
+from nltk.corpus import stopwords
 
 # This is an approach for emotion analysis in the german language
 # created by phuszár
+
+# Variables
+datafolder = Path("data/")
+
 
 class emotions(Enum):
     Ekel = 'Ekel.txt'
@@ -18,31 +24,56 @@ class emotions(Enum):
 
 def tokenize(t):
     # tokenize, erase punctuation(#, $, %, ., ~, ...), erase whitespaces
-    tokens = nltk.tokenize.word_tokenize(t, 'german')
+    t = nltk.tokenize.word_tokenize(t, 'german')
+    return list(filter(None, [''.join(i for i in s if i not in string.punctuation)
+                              for s in t]))
+
+
+def tokenize_and_stem(x):
+    # CISTEM Stemmer
+    tokens = nltk.tokenize.word_tokenize(x, 'german')
     tokens = [''.join(i for i in s if i not in string.punctuation)
               for s in tokens]
-    return tokens
 
-
-def stemmer(s):
-    # CISTEM Stemmer
-    tokens = tokenize(s)
     stemmer = nltk.stem.cistem.Cistem()
     stemmedTokens = [''.join(stemmer.stem(t))
                      for t in tokens]
     return stemmedTokens
 
 
+def stopword_filter(query):
+    stopWords = set(stopwords.words('german'))
+    return list(filter(None, [''.join(q for q in t if t not in stopWords)
+                              for t in query]))
+
+
 def emotion_analysis(query):
-    # ToDo: Analyze the emotion
-    return
+    emotions_d = defaultdict(int)
+    stemmed = tokenize_and_stem(query)
+    swFiltered = stopword_filter(stemmed)
+    for emo in emotions:
+        file_to_open = datafolder / emo.value
+        for sw in swFiltered:
+            with open(file_to_open, 'r', encoding='utf-8') as file:
+                list = [''.join(tokenize_and_stem(t))
+                        for t in file]
+                for l in list:
+                    if sw == l:
+                        print('Found word in: ' + emo.name + '-Dictionary')
+                        emotions_d[emo.name] += 1
+    # ToDo: value with respect to freqeuncy
+    return dict(emotions_d)
 
 
 # for flask
 def main(query):
-    # return a map with percentage of emotion
-    return
+    # :return dict of possible Emotions - dict like a hashmap.
+    # Form like: {'Freude': 6, 'Furcht': 1, 'Ueberraschung': 1, 'Verachtung': 6}
+    emotion_analysis(query)
 
 
 if __name__ == '__main__':
-    print('hello')
+    print('Use test in comments in code')
+
+    testQuery = "Ein dickes Lob an eine Fahrerin von einer Kundin. Durch den starken Schneefall sind an dem Tag mehrere Busse ausgefallen. Viele Leute standen an der Bushaltestelle Neviges Bahnhof. Eine Fahrerin der Linie 649 , bat in der Leitstelle darum, die wartenden Fahrgäste an Ihr Ziel bringen zu können nach Velbert, da der Linienweg des Busses den sie eigentlich fahren sollte, nicht befahrbar war.Die Leitstelle sagte hier zu.Die Fahrgäste waren total glücklich und stellvertretend für alle möchte die Kundin ein dickes Lob aussprechen für die vorausschauende Art und Weise, die diese Fahrerin an den Tag gelegt hat."
+    print(emotion_analysis(testQuery))
