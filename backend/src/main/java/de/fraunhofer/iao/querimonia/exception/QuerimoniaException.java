bf068f3aafb7478d7bdd2d284a5172bd47065111
PaulBredl <st156256@stud.uni-stuttgart.de>
2019-07-09 19:59:21 +0200

    added stack trace for debugging

--- backend/src/main/java/de/fraunhofer/iao/querimonia/exception/QuerimoniaException.java
+++ backend/src/main/java/de/fraunhofer/iao/querimonia/exception/QuerimoniaException.java
@@ -2,6 +2,7 @@ package de.fraunhofer.iao.querimonia.exception;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -120,9 +121,14 @@ public class QuerimoniaException extends ResponseStatusException {
   }
 
   @Override
-  //@JsonIgnore
+  @JsonIgnore
   public StackTraceElement[] getStackTrace() {
     return super.getStackTrace();
+  }
+
+  @JsonProperty("stacktrace")
+  public String stacktraceString() {
+    return ExceptionUtils.getStackTrace(this);
   }
 
 }
