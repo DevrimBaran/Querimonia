b8ce853ffa8c67b10265b59a4355fcba31fae118
Samuel Gigliotti <st157598@stud.uni-stuttgart.de>
2019-07-23 17:46:20 +0200

    Integration der Emotionsanalyse aus python

--- backend/src/main/java/de/fraunhofer/iao/querimonia/nlp/sentiment/FlaskSentiment.java
+++ backend/src/main/java/de/fraunhofer/iao/querimonia/nlp/sentiment/FlaskSentiment.java
@@ -18,22 +18,16 @@ public class FlaskSentiment implements SentimentAnalyzer {
 
   @Override
   public ComplaintProperty analyzeEmotion(String text) {
-    double sentimentValue = analyzeSentiment(text);
-
-    Map<String, Double> result = new HashMap<>();
-
-    if (sentimentValue >= 1) {
-      result.put("Euphorie", 1.0);
-    } else if (sentimentValue >= 0.5) {
-      result.put("Freude", 1.0);
-    } else if (sentimentValue >= 0) {
-      result.put("Neutral", 1.0);
-    } else if (sentimentValue >= -0.5) {
-      result.put("Unzufriedenheit", 1.0);
-    } else {
-      result.put("Wut", 1.0);
+    JSONObject jsonText = new JSONObject();
+    try {
+      jsonText.put("text", text);
+    } catch (JSONException e) {
+      e.printStackTrace();
     }
-    return new ComplaintProperty(result, "Emotion");
+
+    Map<String, Double> flaskResult = FlaskContact.receiveJson(jsonText, "emotion_analyse");
+
+    return new ComplaintProperty(flaskResult, "Emotion");
   }
 
   @Override
