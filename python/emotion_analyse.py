ab26881875fa4f1046a7e0d8b6e4f1c3c82e25c1
SonBoolean <45284935+SonBoolean@users.noreply.github.com>
2019-07-30 16:34:02 +0200

    treebank tokenizer added

--- python/emotion_analyse.py
+++ python/emotion_analyse.py
@@ -1,17 +1,16 @@
 import string
-from nltk.tokenize import word_tokenize
-from nltk import download
 from enum import Enum
+from nltk.data import load
+from nltk.tokenize.treebank import TreebankWordTokenizer
 from pathlib import Path
 from collections import defaultdict
 
 # This is an approach for emotion analysis in the german language
 # created by phuszár
 
-# intial loading for nltk
-download('punkt')
-# Variables
-datafolder = Path("data/")
+
+# Constants
+datafolder = Path('data/')
 
 
 class emotions(Enum):
@@ -24,23 +23,22 @@ class emotions(Enum):
     Wut = 'Wut.txt'
 
 
-def tokenize_and_stem(x):
-    tokens = word_tokenize(x, 'german')
+def treebank_tokenizer(sentence):
+    tokenizer = load('data/german.pickle')
+    treebank_word_tokenize = TreebankWordTokenizer().tokenize
+    tokens = []
+    for s in tokenizer.tokenize(sentence):
+        tokens.extend([token for token in treebank_word_tokenize(s)])
     tokens = [''.join(i for i in s if i not in string.punctuation)
               for s in tokens]
     tokens = list(filter(None, tokens))
-    # CISTEM Stemmer
-    # Results arent better with stemmer, 'Gefahr' and 'Fahrer' are equal after stemming
-    # stemmer = nltk.stem.cistem.Cistem()
-    # stemmedTokens = [''.join(stemmer.stem(t))
-    #                 for t in tokens]
     return tokens
 
 
 def stopword_filter(query):
     file_to_open = datafolder / 'stopWords.txt'
     f = open(file_to_open, 'r')
-    stopWords = [''.join(tokenize_and_stem(t))
+    stopWords = [''.join(treebank_tokenizer(t))
                  for t in f]
     for i, s in enumerate(stopWords):
         stopWords[i] = s.strip()
@@ -60,13 +58,12 @@ def prozent(dict):
 
 def emotion_analysis(query):
     emotions_d = defaultdict(int)
-    stemNtokenize = tokenize_and_stem(query)
-    swFiltered = stopword_filter(stemNtokenize)
-    print(swFiltered)
+    tokens = treebank_tokenizer(query)
+    swFiltered = stopword_filter(tokens)
     for emo in emotions:
         file_to_open = datafolder / emo.value
         with open(file_to_open, 'r', encoding='utf-8') as file:
-            emoList = [''.join(tokenize_and_stem(t))
+            emoList = [''.join(treebank_tokenizer(t))
                        for t in file]
             # delete duplicates happend due stemming
             emoList = list(dict.fromkeys(emoList))
@@ -87,5 +84,5 @@ def main(query):
 if __name__ == '__main__':
     print('Use test in comments in code')
 
-    # testQuery = 'In Ihrem Schreibenvom 8. 2. 2013schreiben Sie, nur "ein" Bus der Linie CE62 von Wuppertal-Elberfeld nach Wuppertal-Ronsdorf sei ausgefallen. Dies kann so nicht stimmen. Vielmehr stellt sich mir und anderen die Frage, weshalbständig undgehäuft Busliniender WSWausfallen undFahrten entgegen dem Fahrplan nicht angeboten werden. Als Rad-, Bahn- und gelegentlicher Autofahrer benutze ich eher selten die Busse der WSW. Wenn ichsie benutze, ergeben sich auffällig häufig Ausfälle und deutliche Verspätungen. Dies betrifft auffallend oft die als schnell konzipierten CE-Linien, nach meiner jüngeren Erfahrung CE61/CE62, weniger auch CE64/CE65 (Elberfeld-Cronenberg), und Nebenlinienwie etwa 643. Einzelne Ihrer Mitarbeiter berichten mir übereinstimmend, die Personaldecke sei zu dünn, um Ausfälle einzelner Mitarbeiter auszugleichen. Wegen spürbaren Personalmangels - nicht, wie Sie schreiben, vereinzelt wegen Krankheit - würden häufig Fahrten ausfallen. Viele Fahrer berichten anschaulich, dass in manchen Stoßzeiten vergeblich wartende Passagiere an Haltestellen stünden. Teilweise berichtet man mir von gezielten Maßnahmen, um Einsparungen herbeizuführen sowie nicht ausgeglichenen Überstunden. Ihre Fahrer sehen dies unmittelbar mit der Politikder WSW verknüpft, die CE-Linien ab März/April 2013auszudünnen.Der Vorgang, auf den sich Ihr Schreiben bezieht, lief tatsächlich so ab: Am 1. 2. 2013 wartete ich an der Morianstraße in Wuppertalmit meinem x Monate alten Sohn nachmittags auf eine Busverbindung nach Ronsdorf, Haltestelle Kniprodestraße. Ich beabsichtigte, den Bus CE62 um 15:47 Uhr zu nehmen. Wir waren auf dem Weg zu einer Nachmittagsveranstaltung für Eltern mit Kleinstkindern, die ich verpasste:Die CE62um 15:47 Uhrfuhr nicht, ebenso nicht die nachfolgenden Busse der Linie CE62:- 16:07 Uhr- 16:27 UhrEs fuhren ferner nicht die Busse der Linie 620um- 15:55 Uhr- 16:15 Uhr- 16:35 Uhrdie Haltestelle an. Ihr Schreiben legt nahe, dass es sich nur um vereinzelte Fälle handele. Unmittelbar bei meinem nächsten Fahrtantritt mit der WSW eine Woche später kam es wiederum zu einem Ausfall. Am Freitag, 8. 2. 2013, fiel Linie CE61 um 15:38 Uhran der Haltestelle Am Stadtbahnhof in Richtung Barmenaus. Mit ca. 25 MinutenVerspätung erreichte ich mit der Linie 640(ab 15:48 Uhr) gegen 16:25 Uhrden Alten Markt in Barmen.Heute, am 21. 2. 2013, fielen die Busse der Linie CE64von Wuppertal Hbf in Richtung Cronenbergum 06:40 Uhr und um 07:00 Uhr aus. Ein Fahrgast erreichte deshalb nicht pünktlich seine Arbeitsstelle in Solingen. Würde ich häufiger fahren, würdeich vermutlich noch häufiger dadurch Zeit verlieren. Ich bitte Sie daher freundlich, Ihre Dienstleistungen bestmöglich fahrplangemäß anzubieten.'
-    # main(testQuery)
+    testQuery = 'In Ihrem Schreiben vom 8. 2. 2013 schreiben Sie, nur "ein" Bus der Linie CE62 von Wuppertal-Elberfeld nach Wuppertal-Ronsdorf sei ausgefallen. Dies kann so nicht stimmen. Vielmehr stellt sich mir und anderen die Frage, weshalbständig undgehäuft Busliniender WSWausfallen undFahrten entgegen dem Fahrplan nicht angeboten werden. Als Rad-, Bahn- und gelegentlicher Autofahrer benutze ich eher selten die Busse der WSW. Wenn ichsie benutze, ergeben sich auffällig häufig Ausfälle und deutliche Verspätungen. Dies betrifft auffallend oft die als schnell konzipierten CE-Linien, nach meiner jüngeren Erfahrung CE61/CE62, weniger auch CE64/CE65 (Elberfeld-Cronenberg), und Nebenlinienwie etwa 643. Einzelne Ihrer Mitarbeiter berichten mir übereinstimmend, die Personaldecke sei zu dünn, um Ausfälle einzelner Mitarbeiter auszugleichen. Wegen spürbaren Personalmangels - nicht, wie Sie schreiben, vereinzelt wegen Krankheit - würden häufig Fahrten ausfallen. Viele Fahrer berichten anschaulich, dass in manchen Stoßzeiten vergeblich wartende Passagiere an Haltestellen stünden. Teilweise berichtet man mir von gezielten Maßnahmen, um Einsparungen herbeizuführen sowie nicht ausgeglichenen Überstunden. Ihre Fahrer sehen dies unmittelbar mit der Politikder WSW verknüpft, die CE-Linien ab März/April 2013auszudünnen.Der Vorgang, auf den sich Ihr Schreiben bezieht, lief tatsächlich so ab: Am 1. 2. 2013 wartete ich an der Morianstraße in Wuppertalmit meinem x Monate alten Sohn nachmittags auf eine Busverbindung nach Ronsdorf, Haltestelle Kniprodestraße. Ich beabsichtigte, den Bus CE62 um 15:47 Uhr zu nehmen. Wir waren auf dem Weg zu einer Nachmittagsveranstaltung für Eltern mit Kleinstkindern, die ich verpasste:Die CE62um 15:47 Uhrfuhr nicht, ebenso nicht die nachfolgenden Busse der Linie CE62:- 16:07 Uhr- 16:27 UhrEs fuhren ferner nicht die Busse der Linie 620um- 15:55 Uhr- 16:15 Uhr- 16:35 Uhrdie Haltestelle an. Ihr Schreiben legt nahe, dass es sich nur um vereinzelte Fälle handele. Unmittelbar bei meinem nächsten Fahrtantritt mit der WSW eine Woche später kam es wiederum zu einem Ausfall. Am Freitag, 8. 2. 2013, fiel Linie CE61 um 15:38 Uhran der Haltestelle Am Stadtbahnhof in Richtung Barmenaus. Mit ca. 25 MinutenVerspätung erreichte ich mit der Linie 640(ab 15:48 Uhr) gegen 16:25 Uhrden Alten Markt in Barmen.Heute, am 21. 2. 2013, fielen die Busse der Linie CE64von Wuppertal Hbf in Richtung Cronenbergum 06:40 Uhr und um 07:00 Uhr aus. Ein Fahrgast erreichte deshalb nicht pünktlich seine Arbeitsstelle in Solingen. Würde ich häufiger fahren, würdeich vermutlich noch häufiger dadurch Zeit verlieren. Ich bitte Sie daher freundlich, Ihre Dienstleistungen bestmöglich fahrplangemäß anzubieten.'
+    main(testQuery)
