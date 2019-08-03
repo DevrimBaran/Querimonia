7476e6a1ef9fa6d7986619d21f7e4832cbbf7d3d
Pascal <st144276@stud.uni-stuttgart.de>
2019-08-03 15:50:02 +0200

    added treebank_tokenizer to senti script

--- python/sentiment_analyse.py
+++ python/sentiment_analyse.py
@@ -1,6 +1,8 @@
 import string
 import sys
 from nltk.tokenize import word_tokenize
+from nltk.data import load
+from nltk.tokenize.treebank import TreebankWordTokenizer
 from pathlib import Path
 
 # Sarcasm, Irony not detected. For example: 'Ich hasse dich und liebe dich nicht' has value > 0
@@ -20,9 +22,21 @@ def negative_sentiment(query):
     return analyze(query, 'SentiWS_v2.0_Negative.txt')
 
 
+def treebank_tokenizer(sentence):
+    tokenizer = load('data/german.pickle')
+    treebank_word_tokenize = TreebankWordTokenizer().tokenize
+    tokens = []
+    for s in tokenizer.tokenize(sentence):
+        tokens.extend([token for token in treebank_word_tokenize(s)])
+    tokens = [''.join(i for i in s if i not in string.punctuation)
+              for s in tokens]
+    tokens = list(filter(None, tokens))
+    return tokens
+
+
 # Check for matches in Dictionaries
 def analyze(query, dict):
-    tokens = filter_query(query)
+    tokens = treebank_tokenizer(query)
     tokens = stopword_filter(tokens)
     dict = datafolder / dict
     sentiment_value = 0
@@ -48,15 +62,6 @@ def analyze(query, dict):
                         sentiment_value += value
 
     return sentiment_value
-
-
-def filter_query(query):
-    # tokenize, erase punctuation(#, $, %, ., ~, ...), erase whitespaces
-    tokens = word_tokenize(query, 'german')
-    tokens = [''.join(i for i in s if i not in string.punctuation)
-              for s in tokens]
-    tokens = list(filter(None, tokens))
-    return tokens
 
 
 def stopword_filter(query):
@@ -117,12 +122,12 @@ if __name__ == '__main__':
     print('In cmd " python sentiment_analyse.py *query* \n or use examples in comments')
 
     # for example
-    # test = ' Sehr geehrte Damen und Herren,der vorstehende Bus ist - mal wieder - ausgefallen. Gestern fiel der Bus um 08.07 aus. Ich hoffe, dass ich auf die heutige Mail von Ihnen auch einmal eine Antwort erhalte, die letzten Hinweise auf Ausfälle der Linie 603 wurden von Ihnen ignoriert. Ich habe wirklich kein Verständnis mehr für die dauernden Ausfälle der Linie 603. Gehen Sie bitte davon aus, dass es durchaus Kunden gibt, die zur Arbeit müssen oder einen Zug erreichen müssen. Ich erwarte für die regelmäßigen Fahrpreiserhöhungen auch die entsprechende Leistung Ihrerseits!!!Ich erwarte nunmehr eine Antwort und ein Angebot der Entschädigung. Vielen Dank.'
+    # test = 'Sehr geehrte Damen und Herren,der vorstehende Bus ist - mal wieder - ausgefallen. Gestern fiel der Bus um 08.07 aus. Ich hoffe, dass ich auf die heutige Mail von Ihnen auch einmal eine Antwort erhalte, die letzten Hinweise auf Ausfälle der Linie 603 wurden von Ihnen ignoriert. Ich habe wirklich kein Verständnis mehr für die dauernden Ausfälle der Linie 603. Gehen Sie bitte davon aus, dass es durchaus Kunden gibt, die zur Arbeit müssen oder einen Zug erreichen müssen. Ich erwarte für die regelmäßigen Fahrpreiserhöhungen auch die entsprechende Leistung Ihrerseits!!!Ich erwarte nunmehr eine Antwort und ein Angebot der Entschädigung. Vielen Dank.'
     # test_pos = 'Ich liebe Frieden und finde Blumen schön'
     # test_neg = 'Ich mag Zerstörung und Gewalt und hasse Glück'
     # neg_pos = 'Ich mag keine Pünktlichkeit und mag Höflichkeit nicht'
     # neg_neg = 'Ich mag keine Verspätungen und finde Ausfälle nicht schön'
 
-    # print(main(neg_neg))
+    # print(main(test))
     # for cmd
     # print("Sentiment Value is: " + (str)(parse_arg()))
